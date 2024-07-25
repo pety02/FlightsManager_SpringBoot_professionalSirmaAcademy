c128ae4a597167d0e0db61126d8678b269e50592
pety02 <pety02@abv.bg>
2024-07-25 16:21:27 +0300

    Init commit

---
+++ src/main/java/com/example/flightsmanager/FlightsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.flightsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightsManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flightsmanager/FlightsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flightsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
