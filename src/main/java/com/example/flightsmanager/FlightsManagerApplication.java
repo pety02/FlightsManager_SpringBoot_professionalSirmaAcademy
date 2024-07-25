c9fdffd5af014ef0bb89cbf43986ac6c8b0a3a88
pety02 <pety02@abv.bg>
2024-07-25 16:29:32 +0300

    Init commit

--- src/main/java/com/example/flightsmanager/FlightsManagerApplication.java
+++ src/main/java/com/example/flightsmanager/FlightsManagerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlightsManagerApplication {
-    // comment
     public static void main(String[] args) {
         SpringApplication.run(FlightsManagerApplication.class, args);
     }
